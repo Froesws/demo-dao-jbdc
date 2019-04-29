c6711d2fd7bf181c2aea7703e47e9e8a2a91dece
William Froes <froes.ws@gmail.com>
2019-04-29 16:38:36 -0300

    Update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,14 +13,22 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao =  DaoFactory.createSellerDao();
-		Seller seller = sellerDao.findById(3);
-		System.out.println(seller);
+		Department dep = new Department(2, null);
+		//Seller newseller = new Seller (null, "Greg Pinto","greg@gmail.com", new Date(), 4000.00, dep);
+		//sellerDao.insert(newseller);
+		//System.out.println(newseller.getId());
 		
+		 Seller seller = sellerDao.findById(3);
+		 seller.setName("Jacinto Barros do Rego");
+		 sellerDao.update(seller);
+		 
 		List <Seller> list = sellerDao.findAll();
 		
 		for (Seller obj: list) {
 			System.out.println(obj);
 		}
+		
+		
 
 	}
 
